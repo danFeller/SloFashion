9264b0030101483c2285740bf526e390dd19432b
Mohnish Kalia <mohnish.kalia@gmail.com>
2023-11-15 23:38:03 -0600

    Working enter dialogue

--- app/src/main/java/com/example/slofashion/DialogueActivity.java
+++ app/src/main/java/com/example/slofashion/DialogueActivity.java
@@ -4,40 +4,60 @@ import static java.util.UUID.randomUUID;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.slofashion.datamodels.Author;
 import com.example.slofashion.datamodels.DialogueAction;
 import com.example.slofashion.datamodels.Message;
+import com.google.android.material.slider.RangeSlider;
 import com.stfalcon.chatkit.messages.MessagesList;
 import com.stfalcon.chatkit.messages.MessagesListAdapter;
 
+import java.util.Arrays;
 import java.util.Date;
 
-public class DialogueActivity extends AppCompatActivity {
+public class DialogueActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /** Invoke different conversations based on DialogueType */
     enum DialogueType {
         ENTER_STORE,
         LEAVE_STORE
     }
 
+    // View fields
     MessagesList messagesList;
-    MessagesListAdapter<Message> adapter;
-    DialogueType dialogueType;
-    int dialogueStep;
+    FrameLayout inputContainer;
+    Button dialogueDoneBtn;
 
+    // View inputs
+    RangeSlider moneyInputSlider;
+    Button moneySubmitBtn;
+    Button confirmYesBtn;
+    Button confirmNoBtn;
+
+    // Adapter to update list
+    MessagesListAdapter<Message> adapter;
+
+    // Dialogue action fields
     final DialogueAction[] enterDialogue = new DialogueAction[] {
-            DialogueAction.GenChatbotAction("You have $x in your budget today"),
+            DialogueAction.GenChatbotAction("You have $%s left in your budget"),
             DialogueAction.GenChatbotAction("How much are you spending today?"),
-            new DialogueAction(true, null, 0, "money"),
-            DialogueAction.GenChatbotAction("Filler 1"),
-            DialogueAction.GenChatbotAction("You will be left with $y in the budget after this trip"),
-            DialogueAction.GenChatbotAction("Filler 2"),
+            new DialogueAction(true, null, 0, DialogueAction.InputType.MONEY),
+            DialogueAction.GenChatbotAction("You will be left with $%s in the budget after this trip"),
+            new DialogueAction(true, null, -4, DialogueAction.InputType.CONFIRM),
+            DialogueAction.GenChatbotAction("How many clothes are you purchasing today?"),
+            new DialogueAction(true, null, 0, DialogueAction.InputType.MONEY),
+            DialogueAction.GenChatbotAction("You will be left with %s clothes left in the budget after this trip"),
+            new DialogueAction(true, null, -4, DialogueAction.InputType.CONFIRM),
     };
     final DialogueAction[] exitDialogue = new DialogueAction[] {
-            DialogueAction.GenChatbotAction("You have $x in your budget today"),
+            DialogueAction.GenChatbotAction("You have $%s left in your budget"),
             DialogueAction.GenChatbotAction("How much did you spend?"),
-            new DialogueAction(true, null, 0, "money"),
+            new DialogueAction(true, null, 0, DialogueAction.InputType.MONEY),
             DialogueAction.GenChatbotAction("Filler 3"),
             DialogueAction.GenChatbotAction("Done. You will be left with $y in the budget after this trip"),
             DialogueAction.GenChatbotAction("Filler 4"),
@@ -46,12 +66,17 @@ public class DialogueActivity extends AppCompatActivity {
     final Author chatbotAuthor = new Author("chatbot", "chatbot");
     final Author userAuthor = new Author("user", "user");
 
+    DialogueAction[] targetConversation;
+    DialogueType dialogueType;
+    int dialogueStep;
+    int moneyInputValue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Intent intent = getIntent();
-        String strDialogueType = intent.getStringExtra(Intent.EXTRA_REFERRER_NAME);
+        String strDialogueType = intent.getStringExtra(Intent.EXTRA_TEXT);
         try {
             dialogueType = DialogueType.valueOf(strDialogueType);
         } catch (IllegalArgumentException | NullPointerException e) {
@@ -60,33 +85,110 @@ public class DialogueActivity extends AppCompatActivity {
         }
 
         dialogueStep = 0;
+        moneyInputValue = 0;
 
         setContentView(R.layout.activity_dialogue);
         messagesList = findViewById(R.id.messagesList);
+        inputContainer = findViewById(R.id.input_container);
+
+        dialogueDoneBtn = findViewById(R.id.dialogue_done_view_budget);
+
+        moneyInputSlider = findViewById(R.id.money_input_slider);
+        moneySubmitBtn = findViewById(R.id.money_submit);
+        confirmYesBtn = findViewById(R.id.confirm_yes);
+        confirmNoBtn = findViewById(R.id.confirm_no);
+
+        dialogueDoneBtn.setOnClickListener(view -> {
+            Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(i);
+        });
+
+        moneyInputSlider.addOnChangeListener((slider, value, fromUser) -> moneyInputValue = Math.round(value));
+        moneySubmitBtn.setOnClickListener(this);
+        confirmYesBtn.setOnClickListener(this);
+        confirmNoBtn.setOnClickListener(this);
 
         adapter = new MessagesListAdapter<>("user", null);
         messagesList.setAdapter(adapter);
 
-        DialogueAction[] targetConversation = dialogueType.equals(DialogueType.ENTER_STORE)
-                ? enterDialogue
-                : exitDialogue;
-        goToNextUserDialogue(targetConversation);
+        switch (dialogueType) {
+            case ENTER_STORE:
+                targetConversation = enterDialogue;
+                break;
+            case LEAVE_STORE:
+                targetConversation = exitDialogue;
+                break;
+            default:
+                targetConversation = null;
+                break;
+        }
+
+        goToNextUserDialogue();
     }
 
-    protected void goToNextUserDialogue(DialogueAction[] conversation) {
+    protected void goToNextUserDialogue() {
+        DialogueAction[] conversation = targetConversation;
         for (; dialogueStep < conversation.length && !conversation[dialogueStep].userAction; dialogueStep++) {
             DialogueAction da = conversation[dialogueStep];
             adapter.addToStart(new Message(randomUUID().toString(), da.promptText, chatbotAuthor, new Date()), true);
         }
-        if (dialogueStep >= conversation.length)
-            adapter.addToStart(new Message(randomUUID().toString(), "View your budget now", chatbotAuthor, new Date()), true);
 
-        // TODO: currently simulate user interaction, should just create input element and pick up logic at onSubmit handler
+        if (dialogueStep >= conversation.length) {
+            // if no more user dialogue
+            adapter.addToStart(new Message(randomUUID().toString(), "View the changes in your budget", chatbotAuthor, new Date()), true);
+            findViewById(R.id.money_input_layout).setVisibility(View.GONE);
+            findViewById(R.id.confirm_input_layout).setVisibility(View.GONE);
+            findViewById(R.id.dialogue_done_view_budget).setVisibility(View.VISIBLE);
+        } else {
+            // else setup input methods
+            DialogueAction curStep = conversation[dialogueStep];
+            switch (curStep.inputType) {
+                case MONEY:
+                    findViewById(R.id.money_input_layout).setVisibility(View.VISIBLE);
+                    findViewById(R.id.confirm_input_layout).setVisibility(View.GONE);
+                    break;
+                case CONFIRM:
+                    findViewById(R.id.money_input_layout).setVisibility(View.GONE);
+                    findViewById(R.id.confirm_input_layout).setVisibility(View.VISIBLE);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int[] validSubmitBtns = new int[] { moneySubmitBtn.getId(), confirmYesBtn.getId(), confirmNoBtn.getId() };
+        boolean validSubmit = Arrays.stream(validSubmitBtns).anyMatch(b -> b == view.getId());
+
+        if (!validSubmit) return;
+
+        DialogueAction[] conversation = targetConversation;
         if (dialogueStep < conversation.length && conversation[dialogueStep].userAction) {
-            adapter.addToStart(new Message(randomUUID().toString(), "$25", userAuthor, new Date()), true);
+            DialogueAction curStep = conversation[dialogueStep];
+            boolean isNoBtn = view.getId() == confirmNoBtn.getId();
+
+            String msgText = "";
+            switch (curStep.inputType) {
+                case MONEY:
+                    msgText = "" + moneyInputValue;
+                    break;
+                case CONFIRM:
+                    msgText = "" + (isNoBtn ? confirmNoBtn.getText() : confirmYesBtn.getText());
+                    break;
+            }
+
+            Message userMsg = new Message(randomUUID().toString(), msgText, userAuthor, new Date());
+            adapter.addToStart(userMsg, true);
+
+            if (dialogueType == DialogueType.LEAVE_STORE) {
+                // TODO: add actual persistence logic here based on values
+            }
+
+            if (isNoBtn)
+                dialogueStep += curStep.dialogueSkip;
             dialogueStep++;
         }
-        if (dialogueStep < conversation.length)
-            goToNextUserDialogue(conversation);
+
+        goToNextUserDialogue();
     }
 }
--- app/src/main/java/com/example/slofashion/datamodels/DialogueAction.java
+++ app/src/main/java/com/example/slofashion/datamodels/DialogueAction.java
@@ -1,12 +1,29 @@
 package com.example.slofashion.datamodels;
 
 public class DialogueAction {
-    public boolean userAction;
-    public String promptText;
-    public int dialogueSkip;
-    public String inputType;
+    public enum InputType {
+        MONEY,
+        CONFIRM,
+    }
 
-    public DialogueAction(boolean userAction, String promptText, int dialogueSkip, String inputType) {
+    /**
+     * Whether or not this is a user action or a chatbot action
+     */
+    public boolean userAction;
+    /**
+     * The actual message text to display
+     */
+    public String promptText;
+    /**
+     * If inputType is CONFIRM, selecting the no button will change dialog step by this much
+     */
+    public int dialogueSkip;
+    /**
+     * The type of input the user needs to give
+     */
+    public InputType inputType;
+
+    public DialogueAction(boolean userAction, String promptText, int dialogueSkip, InputType inputType) {
         this.userAction = userAction;
         this.promptText = promptText;
         this.dialogueSkip = dialogueSkip;
